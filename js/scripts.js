daff9ce57fb8887c3b23c15a4d448aa30c26f5a9
Michelle David <hilarity.ensues@gmail.com>
2020-07-16 14:50:17 -0700

    update css and text

--- js/scripts.js
+++ js/scripts.js
@@ -18,22 +18,21 @@ $(document).ready(function() {
     const total = answer1 + answer2 + answer3 + answer4 + answer5;
     let result;
     if (total <= 10) {
-      result = "congrats! option one."
+      result = (username + ", you're a standout candidate to master Python! this high-level, general-purpose programming language will allow you to tackle the greatest existential problems of all time.")
     } else if (total >= 11 && total <= 30) {
-      result = "congrats! option two."
+      result = username + ", amazing! all signs point to learning Swift. once you've memorized the ins and outs of this exciting new language you'll be able to create the next candy crush! "
     } else if (total >= 31 && total <= 50) {
-      result = "congrats! option three."
+      result = username + ", nice job. our survey says you're a top candidate for a certificate in Rust. voted the 'most loved programming language' in the Stack Overflow Developer Survey every year since 2016, this versatile language is free, open-source, and just might save the world."
     } else if (total >= 51 && total <= 80) {
-      result = "congrats! option four."
+      result = username + ", beep boop, our algorithim suggests SQL! this domain-specific langauge is used across many disciplines, including data science. help build a better Netflix recomendation engine in no time once you've mastered this popular language!"
     } else {
-      result = "congrats! option five."
+      result = username + ", our crystal ball sees Visual Basic in your future. first appearing nearly 30 years ago, this event-driven programming language has lasted for a while because of its ease of use and popularity. give it a spin and see for yourself!"
     }
     console.log(total); // remove this after verifying
     console.log(result); // remove this after verifying
 
     $("#output").text(result);
     $("#footer").show();
-    $()
 
   });
 
@@ -44,3 +43,4 @@ $(document).ready(function() {
 });
 
 //don't forget your readme!!!
+// to do for wednesday: add css styles, complete survey result output, add readme.
